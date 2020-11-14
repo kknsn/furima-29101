86244bf8bb8e370b5640c7606d0ab1532ec898f5
kknsn <hrmc5@yahoo.co.jp>
2020-11-14 13:22:58 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, inclusion: { in: @ }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :family_name
+    validates :first_kana
+    validates :family_kana
+  end
+  
+  validates :birthday, presence: true
 end
