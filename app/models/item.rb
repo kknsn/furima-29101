6a14dc8481697eadc296c65e5d148ff3b9404063
kknsn <hrmc5@yahoo.co.jp>
2020-12-12 15:24:24 +0900

    delivery_feeモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
+  belongs_to :delivery_fee
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :state_id
+    validates :delivery_fee_id
   end
   validates :name, :description, :price, presence: true
 end
