61089e65c6a782aef8ec531870c654dda988d2e7
kknsn <hrmc5@yahoo.co.jp>
2020-12-12 14:34:16 +0900

    categoryモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :genre_id, numericality: { other_than: 1 } 
+  validates :name, :description, :price, presence: true
 end
