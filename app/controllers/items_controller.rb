96facdeb60e449d30768bde88d48655d0b35f2d9
kknsn <hrmc5@yahoo.co.jp>
2020-12-12 17:54:49 +0900

    出品商品の保存機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,21 @@ class ItemsController < ApplicationController
 
   def new
     authenticate_user!
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:state_id,:area_id,:delivery_fee_id,:day_id,:price)
   end
 end
