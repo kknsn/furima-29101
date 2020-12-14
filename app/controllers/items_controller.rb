4a8a58867f45ca11661041f6a28fa59409ad25ef
kknsn <hrmc5@yahoo.co.jp>
2020-12-14 11:44:50 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:description,:category_id,:state_id,:area_id,:delivery_fee_id,:day_id,:price)
+    params.require(:item).permit(:name,:description,:category_id,:state_id,:area_id,:delivery_fee_id,:day_id,:price,:image)
   end
 end
