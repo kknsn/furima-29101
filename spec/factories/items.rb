e64384dae821c97d0b2c2003f3085ae5c407796d
kknsn <hrmc5@yahoo.co.jp>
2020-12-24 11:40:42 +0900

    specの文字列を数値に修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name               {"商品名"}
     description        {"説明"}
-    category_id        {"2"}
-    state_id           {"2"}
-    delivery_fee_id    {"2"}
-    area_id            {"2"}
-    day_id             {"2"}
-    price              {"1000"}
+    category_id        {2}
+    state_id           {2}
+    delivery_fee_id    {2}
+    area_id            {2}
+    day_id             {2}
+    price              {1000}
 
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it "priceが300円〜9999999円の間であれば保存できる" do
-        @item.price = "9999999"
+        @item.price = 9999999
         expect(@item).to be_valid
       end
     end
@@ -34,27 +34,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Description can't be blank"
       end
       it "category_idが1だと登録できない" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category must be other than 1"
       end
       it "state_idが1だと登録できない" do
-        @item.state_id = "1"
+        @item.state_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "State must be other than 1"
       end
       it "delivery_fee_idが1だと登録できない" do
-        @item.delivery_fee_id = "1"
+        @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Delivery fee must be other than 1"
       end
       it "area_idが1だと登録できない" do
-        @item.area_id = "1"
+        @item.area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Area must be other than 1"
       end
       it "day_idが1だと登録できない" do
-        @item.day_id = "1"
+        @item.day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Day must be other than 1"
       end
@@ -64,12 +64,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
       it "priceが299円以下であれば登録できない" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be greater than 299"
       end
       it "priceが10000000円以上では登録できない" do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be less than 10000000"
       end
