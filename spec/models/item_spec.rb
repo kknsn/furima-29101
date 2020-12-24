716df0c1888ab1bac5c89f66265cbc3138b5bb05
kknsn <hrmc5@yahoo.co.jp>
2020-12-24 21:31:39 +0900

    itemモデル単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe Item, type: :model do
       it "全ての項目を入力すれば保存できる" do
         expect(@item).to be_valid
       end
-      it "priceが300円〜9999999円の間であれば保存できる" do
-        @item.price = 9999999
-        expect(@item).to be_valid
-      end
     end
 
     context '商品出品がうまくいかないとき' do
@@ -75,6 +71,16 @@ RSpec.describe Item, type: :model do
       end
       it "priceが半角数字以外では登録できない" do
         @item.price = "５００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it "priceが半角英数混合では登録できない" do
+        @item.price = "1000a"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it "priceが半角英語だけでは登録できない" do
+        @item.price = "aaa"
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
