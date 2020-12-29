a74ae7f56e29086fb46b16331685817bc36052f1
kknsn <hrmc5@yahoo.co.jp>
2020-12-29 20:50:11 +0900

    商品出品者以外はトップページに遷移させる記述の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items =Item.all.order("created_at DESC")
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
