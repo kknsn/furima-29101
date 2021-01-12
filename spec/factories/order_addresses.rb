e5f0d995da57578189f1b184ca2436bb3a5e0134
kknsn <hrmc5@yahoo.co.jp>
2021-01-12 23:31:40 +0900

    order_addressモデルの住所情報のテストコード

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code        {"123-4567"}
+    prefecture_id      {2}
+    local              {"横浜市緑区"}
+    house_num          {"青山1-1-1"}
+    building_name      {"ビル303"}
+    tel_num            {"09012345678"}
+  end
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
+
+  describe '商品購入機能' do
+    context '商品購入がうまくいくとき' do
+      it "building_name以外のの項目を全て入力すれば保存できる" do
+        @order_address.building_name = ""
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '商品購入がうまくいかないとき' do
+      it "postal_codeが空だと登録できない" do
+        @order_address.postal_code = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Postal code can't be blank"
+      end
+      it "postal_codeにハイフンが入らなければ登録できない" do
+        @order_address.postal_code = "1234567"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Postal code is invalid"
+      end
+      it "prefecture_idが1だと登録できない" do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Prefecture must be other than 1"
+      end
+      it "localが空だと登録できない" do
+        @order_address.local = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Local can't be blank"
+      end
+      it "house_numが空だと登録できない" do
+        @order_address.house_num = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "House num can't be blank"
+      end
+      it "tel_numが空だと登録できない" do
+        @order_address.tel_num = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Tel num can't be blank"
+      end
+      it "tel_numが12桁以上になると登録できない" do
+        @order_address.tel_num = "090123456789"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Tel num is too long (maximum is 11 characters)"
+      end
+      it "tel_numにハイフンが含まれると登録できない" do
+        @order_address.tel_num = "090-1234-5678"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Tel num is invalid"
+      end
+    end
+  end
+end
