a430e5c549e689b2483af6e937d7e6f82c1e11a0
kknsn <hrmc5@yahoo.co.jp>
2021-01-16 00:22:43 +0900

    非ログインユーザーを遷移させる機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_item, only: [:index,:create]
   before_action :sold_out_item, only: [:index]
 
