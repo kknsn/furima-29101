e88ef59ecbe4f85740d834693a09ce05a70715e4
kknsn <hrmc5@yahoo.co.jp>
2021-01-16 16:16:47 +0900

    重複したバリデーションの修正(2)

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,16 +3,17 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :local, :house_num, :building_name, :tel_num, :order_id, :user_id, :item_id,:token
 
   #配送先住所に関するバリデーション
-  validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/}
-  validates :tel_num, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :postal_code, :local, :house_num, :tel_num, presence: true
-  #購入記録に関するバリデーション
   with_options presence: true do
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :tel_num, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 }
+    validates :local
+    validates :house_num
     validates :token
     validates :user_id
     validates :item_id
   end
+  validates :prefecture_id, numericality: { other_than: 1 }
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, local: local, house_num: house_num, building_name: building_name, tel_num: tel_num, order_id: order.id)
